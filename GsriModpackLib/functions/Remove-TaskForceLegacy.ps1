99425c477d0bf77ba3a1cf97d2ed39f1fb5ef3a3
ArwynFr <10365998+ArwynFr@users.noreply.github.com>
2019-08-01 20:33:29 +0200

    Add SupportsShouldProcess to remove tfar

--- GsriModpackLib/functions/Remove-TaskForceLegacy.ps1
+++ GsriModpackLib/functions/Remove-TaskForceLegacy.ps1
@@ -1,10 +1,12 @@
 function Remove-TaskForceLegacy {
-    [CmdletBinding()]
+    [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'Low')]
     Param()
 
     "$env:APPDATA\TS3Client\plugins\task_force_radio_win64.dll",
     "$env:APPDATA\TS3Client\plugins\task_force_radio_win32.dll" | ForEach-Object {
-        Remove-Item $_ -ErrorAction Ignore
-        Write-Verbose "$_ removed"
+        if((Test-Path $_) -and ($PSCmdlet.ShouldProcess($_, 'Remove-Item'))) {
+            Remove-Item $_
+            Write-Verbose "$_ removed"
+        }
     }
 }
