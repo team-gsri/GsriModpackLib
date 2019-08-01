d2352dfde9f95a3d7c994e7c094308673b7f209b
Arwyn <10365998+ArwynFr@users.noreply.github.com>
2019-08-01 15:59:40 +0200

    Add unit tests coverage

--- test.ps1
+++ test.ps1
@@ -1,3 +1,3 @@
 Install-Module Pester -Force
 Import-Module .\GsriModpackLib -Force
-Invoke-Pester .\GsriModpackLib\tests\*.Tests.ps1 -CodeCoverage .\GsriModpackLib\functions\*.ps1 -OutputFile output.xml
+Invoke-Pester .\GsriModpackLib\tests\*.Tests.ps1 -CodeCoverage .\GsriModpackLib\functions\*.ps1 -OutputFile output.xml -CodeCoverageOutputFile coverage.xml
